10be716df0a58cc43184ae9f847154a2bcaa010e
Tania Sanchez <tania.sanchez16@live.com>
2018-09-11 18:32:42 -0500

    Se modifica el template para desplegar los posts de la base de datos

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from django.http import HttpResponse
+from django.template import loader
+from .models import Post
 # Create your views here.
 
 def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	context = {
+		'posts' : posts
+	}
+	return render(request,'blog/post_list.html', context)
